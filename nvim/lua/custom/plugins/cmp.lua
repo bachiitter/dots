e005571a92098fe11178d6d87977f33504864186
nèiliǎn de <143877686+neiilian@users.noreply.github.com>
2023-09-05 03:05:53 -0700

    add lsp kind

--- nvim/lua/custom/plugins/cmp.lua
+++ nvim/lua/custom/plugins/cmp.lua
@@ -3,6 +3,7 @@ return {
   dependencies = {
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
+    'onsails/lspkind.nvim',
 
     -- Adds LSP completion capabilities
     'hrsh7th/cmp-path',
@@ -15,6 +16,34 @@ return {
     require('luasnip.loaders.from_vscode').lazy_load()
     luasnip.config.setup {}
 
+    local kind_icons = {
+      Text = '󰉿',
+      Method = '󰆧',
+      Function = '󰊕',
+      Constructor = '',
+      Field = '󰜢',
+      Variable = '󰀫',
+      Class = '󰠱',
+      Interface = '',
+      Module = '',
+      Property = '󰜢',
+      Unit = '󰑭',
+      Value = '󰎠',
+      Enum = '',
+      Keyword = '󰌋',
+      Snippet = '',
+      Color = '󰏘',
+      File = '󰈙',
+      Reference = '󰈇',
+      Folder = '󰉋',
+      EnumMember = '',
+      Constant = '󰏿',
+      Struct = '󰙅',
+      Event = '',
+      Operator = '󰆕',
+      TypeParameter = '󰅲',
+    }
+
     cmp.setup {
 
       snippet = {
@@ -22,7 +51,14 @@ return {
           luasnip.lsp_expand(args.body)
         end,
       },
-
+      formatting = {
+        format = function(_, vim_item)
+          -- Kind icons
+          vim_item.kind =
+              string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+          return vim_item
+        end,
+      },
       mapping = cmp.mapping.preset.insert {
         ['<C-n>'] = cmp.mapping.select_next_item {
           behavior = cmp.SelectBehavior.Select,

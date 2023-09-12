1314843cb9d045f736bc4bf348448038ac8b40c7
nèiliǎn de <143877686+neiilian@users.noreply.github.com>
2023-09-11 21:35:55 -0700

    switch eslint and prettier with eslint_d and pretttierd

--- nvim/lua/custom/plugins/null-ls.lua
+++ nvim/lua/custom/plugins/null-ls.lua
@@ -6,11 +6,11 @@ return {
     null_ls.setup {
       debug = true,
       sources = {
-        null_ls.builtins.formatting.prettier.with {
+        null_ls.builtins.formatting.prettierd.with {
           extra_filetypes = { 'astro' },
         },
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.code_actions.eslint,
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.code_actions.eslint_d,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.gofumpt,
       },

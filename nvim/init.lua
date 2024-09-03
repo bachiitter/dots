1ced604e8eeb7df1fa508ad75cc57c3a02adc266
Bachitter <bachiitter@pm.me>
2024-09-03 14:41:07 -0700

    disable arrow keys nvim, add kitty config and zed assistant config

--- nvim/init.lua
+++ nvim/init.lua
@@ -75,12 +75,10 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagn
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- TIP: Disable arrow keys in normal mode
--- [[
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
--- ]]
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move left!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move right!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move up!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move down!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -395,7 +393,7 @@ require('lazy').setup({
           end,
         },
         view = {
-          entries = 'native'
+          entries = 'native',
         },
         completion = { completeopt = 'menuone,noinsert' },
         mapping = cmp.mapping.preset.insert {

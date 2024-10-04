7aa63f759ba9a4df231e32e4cd1d66701b086276
Bachitter <143877686+bachiitter@users.noreply.github.com>
2024-10-04 04:53:50 -0700

    Add Wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,35 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Font
+config.font = wezterm.font_with_fallback({
+	"JetBrainsMono Nerd Font",
+})
+
+config.font_size = 11.5
+config.line_height = 1
+config.cell_width = 1
+config.custom_block_glyphs = false
+
+-- Color Scheme
+config.color_scheme = "Catppuccin Mocha"
+
+-- Window
+-- config.window_decorations = "NONE"
+config.window_padding = {
+	left = 2,
+	right = 2,
+	top = 2,
+	bottom = 2,
+}
+
+-- Tab bar
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.show_new_tab_button_in_tab_bar = false
+
+-- and finally, return the configuration to wezterm
+return config

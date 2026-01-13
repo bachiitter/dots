1ca1894e1ce71f2aa7333b7227be0ddab5915c9f
Bachitter <143877686+bachiitter@users.noreply.github.com>
2026-01-13 13:52:18 -0800

    sync

--- nvim/init.lua
+++ nvim/init.lua
@@ -69,7 +69,9 @@ local map = vim.keymap.set
 
 -- General
 map('n', '<Esc>', '<cmd>nohlsearch<CR>')
-map('n', '<leader>e', function() vim.diagnostic.open_float { scope = 'line' } end, { desc = 'Diagnostic float' })
+map('n', '<leader>e', function()
+  vim.diagnostic.open_float { scope = 'line' }
+end, { desc = 'Diagnostic float' })
 map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Diagnostic quickfix' })
 map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
 map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
@@ -81,13 +83,27 @@ map('n', '<C-k>', '<C-w><C-k>', { desc = 'Focus up' })
 map('n', '<leader>ec', '<cmd>e ~/.config/nvim/init.lua<CR>', { desc = 'Edit config' })
 
 -- Search (Snacks picker)
-map('n', '<leader>sf', function() Snacks.picker.git_files() end, { desc = 'Search Files (git)' })
-map('n', '<leader>sa', function() Snacks.picker.files() end, { desc = 'Search All Files' })
-map('n', '<leader>sg', function() Snacks.picker.grep() end, { desc = 'Search Grep' })
-map('n', '<leader>sw', function() Snacks.picker.grep_word() end, { desc = 'Search Word' })
-map('n', '<leader>sd', function() Snacks.picker.diagnostics() end, { desc = 'Search Diagnostics' })
-map('n', '<leader>sr', function() Snacks.picker.resume() end, { desc = 'Search Resume' })
-map('n', '<leader><leader>', function() Snacks.picker.buffers() end, { desc = 'Buffers' })
+map('n', '<leader>sf', function()
+  Snacks.picker.git_files()
+end, { desc = 'Search Files (git)' })
+map('n', '<leader>sa', function()
+  Snacks.picker.files()
+end, { desc = 'Search All Files' })
+map('n', '<leader>sg', function()
+  Snacks.picker.grep()
+end, { desc = 'Search Grep' })
+map('n', '<leader>sw', function()
+  Snacks.picker.grep_word()
+end, { desc = 'Search Word' })
+map('n', '<leader>sd', function()
+  Snacks.picker.diagnostics()
+end, { desc = 'Search Diagnostics' })
+map('n', '<leader>sr', function()
+  Snacks.picker.resume()
+end, { desc = 'Search Resume' })
+map('n', '<leader><leader>', function()
+  Snacks.picker.buffers()
+end, { desc = 'Buffers' })
 
 -- Format
 map({ 'n', 'v' }, '<leader>f', function()
@@ -95,7 +111,9 @@ map({ 'n', 'v' }, '<leader>f', function()
 end, { desc = 'Format buffer' })
 
 -- Oil
-map('n', '<leader>-', function() require('oil').toggle_float() end, { desc = 'Oil' })
+map('n', '<leader>-', function()
+  require('oil').toggle_float()
+end, { desc = 'Oil' })
 
 --------------------------------------------------------------------------------
 -- Autocmds
@@ -106,7 +124,9 @@ local augroup = vim.api.nvim_create_augroup
 autocmd('BufEnter', { command = 'set formatoptions-=cro' })
 autocmd('TextYankPost', {
   group = augroup('highlight-yank', { clear = true }),
-  callback = function() vim.hl.on_yank() end,
+  callback = function()
+    vim.hl.on_yank()
+  end,
 })
 autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = { '*.txt', '*.md', '*.tex' },
@@ -151,21 +171,39 @@ autocmd('LspAttach', {
       map(mode or 'n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
     end
 
-    lsp_map('gd', function() Snacks.picker.lsp_definitions() end, 'Goto Definition')
-    lsp_map('gr', function() Snacks.picker.lsp_references() end, 'Goto References')
-    lsp_map('gi', function() Snacks.picker.lsp_implementations() end, 'Goto Implementation')
+    lsp_map('gd', function()
+      Snacks.picker.lsp_definitions()
+    end, 'Goto Definition')
+    lsp_map('gr', function()
+      Snacks.picker.lsp_references()
+    end, 'Goto References')
+    lsp_map('gi', function()
+      Snacks.picker.lsp_implementations()
+    end, 'Goto Implementation')
     lsp_map('gD', vim.lsp.buf.declaration, 'Goto Declaration')
-    lsp_map('<leader>D', function() Snacks.picker.lsp_type_definitions() end, 'Type Definition')
-    lsp_map('<leader>ds', function() Snacks.picker.lsp_symbols() end, 'Document Symbols')
-    lsp_map('<leader>ws', function() Snacks.picker.lsp_workspace_symbols() end, 'Workspace Symbols')
+    lsp_map('<leader>D', function()
+      Snacks.picker.lsp_type_definitions()
+    end, 'Type Definition')
+    lsp_map('<leader>ds', function()
+      Snacks.picker.lsp_symbols()
+    end, 'Document Symbols')
+    lsp_map('<leader>ws', function()
+      Snacks.picker.lsp_workspace_symbols()
+    end, 'Workspace Symbols')
     lsp_map('<leader>rn', vim.lsp.buf.rename, 'Rename')
     lsp_map('<leader>ca', function()
-      vim.lsp.buf.code_action { filter = function(a) return a.disabled == nil end }
+      vim.lsp.buf.code_action {
+        filter = function(a)
+          return a.disabled == nil
+        end,
+      }
     end, 'Code Action', { 'n', 'v' })
     lsp_map('<C-s>', vim.lsp.buf.signature_help, 'Signature Help')
 
     local client = vim.lsp.get_client_by_id(event.data.client_id)
-    if not client then return end
+    if not client then
+      return
+    end
 
     if client:supports_method('textDocument/documentHighlight', event.buf) then
       local hl_group = augroup('lsp-highlight', { clear = false })
@@ -203,6 +241,7 @@ vim.pack.add {
   { src = 'https://github.com/bachiitter/orng.nvim' },
   { src = 'https://github.com/folke/snacks.nvim' },
   { src = 'https://github.com/lewis6991/gitsigns.nvim' },
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
   { src = 'https://github.com/nvim-mini/mini.nvim' },
   { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
   { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
@@ -220,11 +259,11 @@ require('orng').setup { style = 'dark', transparent = true }
 require('mini.ai').setup { n_lines = 500 }
 require('mini.surround').setup()
 require('mini.pairs').setup()
-require("mini.diff").setup()
+require('mini.diff').setup()
 require('mini.statusline').setup()
 
 -- Icons
- require('nvim-web-devicons').setup {}
+require('nvim-web-devicons').setup {}
 
 -- Snacks (immediate - needed for keybinds)
 require('snacks').setup { input = {}, picker = {} }
@@ -232,8 +271,23 @@ require('snacks').setup { input = {}, picker = {} }
 -- Treesitter
 require('nvim-treesitter').setup {
   ensure_installed = {
-    'astro', 'bash', 'css', 'go', 'gomod', 'gosum', 'gowork', 'html',
-    'javascript', 'json', 'lua', 'luadoc', 'markdown', 'tsx', 'typescript', 'vim', 'vimdoc',
+    'astro',
+    'bash',
+    'css',
+    'go',
+    'gomod',
+    'gosum',
+    'gowork',
+    'html',
+    'javascript',
+    'json',
+    'lua',
+    'luadoc',
+    'markdown',
+    'tsx',
+    'typescript',
+    'vim',
+    'vimdoc',
   },
   auto_install = true,
 }
@@ -252,7 +306,9 @@ require('gitsigns').setup {
 require('conform').setup {
   notify_on_error = false,
   format_on_save = function(bufnr)
-    if vim.tbl_contains({ 'c', 'cpp' }, vim.bo[bufnr].filetype) then return nil end
+    if vim.tbl_contains({ 'c', 'cpp' }, vim.bo[bufnr].filetype) then
+      return nil
+    end
     return { timeout_ms = 500, lsp_format = 'never' }
   end,
   formatters_by_ft = {
@@ -290,8 +346,18 @@ require('blink.cmp').setup {
     ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
     ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
     ['<C-Space>'] = { 'show', 'fallback' },
-    ['<Tab>'] = { function(cmp) return cmp.snippet_active() and cmp.snippet_forward() or cmp.select_next() end, 'fallback' },
-    ['<S-Tab>'] = { function(cmp) return cmp.snippet_active() and cmp.snippet_backward() or cmp.select_prev() end, 'fallback' },
+    ['<Tab>'] = {
+      function(cmp)
+        return cmp.snippet_active() and cmp.snippet_forward() or cmp.select_next()
+      end,
+      'fallback',
+    },
+    ['<S-Tab>'] = {
+      function(cmp)
+        return cmp.snippet_active() and cmp.snippet_backward() or cmp.select_prev()
+      end,
+      'fallback',
+    },
   },
   appearance = { use_nvim_cmp_as_default = false, nerd_font_variant = 'normal' },
   sources = {
